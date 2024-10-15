8da009db61dd73c4c3db857e573240dccceb3173
Xuanli <ludwigchen@proton.me>
2024-10-15 14:05:49 +0800

    Add mercury-default rule

--- config.def.h
+++ config.def.h
@@ -28,6 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class, instance, title, tags, mask, isfloating, monitor */
 	{ "Firefox", NULL, NULL, 1 << 8, 0, -1 },
+	{ "mercury-default", NULL, NULL, 1 << 8, 0, -1 },
 	{ "Zathura", NULL, NULL, 1, 0, -1 },
 	{ "lf", NULL, NULL, 1 << 7, 0, -1 },
 };
